fd36e20b0c8c3885147bc1011cca727d1323f3e1
Akash Kumar <piratedvirus@gmail.com>
2020-08-28 20:59:45 +0530

    Complete Model Training, Deployment and Testing

--- serve/predict.py
+++ serve/predict.py
@@ -69,9 +69,10 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
+    words_ = review_to_words(input_data)
 
-    data_X = None
-    data_len = None
+    data_X, data_len = convert_and_pad(model.word_dict, words_)
+    # data_len = None
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -86,7 +87,7 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
-    result = None
+    with torch.no_grad():
+        result = np.round(model.forward(data).numpy())
 
     return result
--- train/train.py
+++ train/train.py
@@ -68,8 +68,36 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     """
     
     # TODO: Paste the train() method developed in the notebook here.
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: Complete this train method to train the model provided.
+            
+            # zero out the accumulated gradients
+            model.zero_grad()
 
-    pass
+            # calculate the output
+            output = model(batch_X)
+
+            # calculate the loss
+            loss = loss_fn(output, batch_y)
+
+            # perform backprop
+            loss.backward()
+
+            # update the parameters
+            optimizer.step()
+            
+            total_loss += loss.data.item()
+        
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
+
 
 
 if __name__ == '__main__':
